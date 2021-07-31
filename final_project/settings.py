02eecd39581753d0a9939cb0b364b614451f94b8
hamidrezafekri <hamidrezafekri.dev@gmail.com>
2021-07-31 17:03:26 +0430

    create my app in database

--- final_project/settings.py
+++ final_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_project',
+        'USER': 'hamidrezafekri',
+        'PASSWORD': 'hamid14520',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
