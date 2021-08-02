5d1036a025c4c1cc0647d590d9565c472959c8eb
hamidrezafekri <hamidrezafekri.dev@gmail.com>
2021-08-02 23:58:01 +0430

    work on model and other parts

--- product/admin.py
+++ product/admin.py
@@ -1,16 +1,115 @@
 from django.contrib import admin
-
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
-from product.models import Product, Details
+from product.models import Product, Detail, Categori, \
+    DiscountCode, Price, Discount, Brand
+
+
+def logical_delete(modeladmin, request, queryset):
+    queryset.update(deleted=True)
+
+
+logical_delete.short_description = "logical delete"
+
+
+class BrandAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (_('add_brand'),
+         {'fields': ['name']}),
+        (_('delete_status'),
+         {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['name']
+
+    list_filter = ['name']
+
+    actions = [logical_delete]
+
+
+class CategoryAdmid(admin.ModelAdmin):
+    fieldsets = [(_('add catergory'),
+                  {'fields': ['parent', 'name']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+    search_fields = ['parent', 'name']
+    list_filter = ['name', 'parent']
+    actions = [logical_delete]
+
+
+class DetailAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add detail'),
+                  {'fields': ['product_detail', 'feature', 'explain']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['product_detail', 'feature', 'explain']
+    list_filter = ['product_detail', 'feature', 'explain']
+    actions = [logical_delete]
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add discount_code'),
+                  {'fields': ['name', 'code', 'expiry_date']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['name', 'code', 'expiry_date']
+    list_filter = ['name', 'code', 'expiry_date']
+    actions = [logical_delete]
+
+class DiscountAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add discount'),
+                  {'fields': ['name', 'discount', 'expiry_date']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['name', 'discount', 'expiry_date']
+    list_filter = ['name', 'discount', 'expiry_date']
+    actions = [logical_delete]
+
+
+class PriceAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add price'),
+                  {'fields': ['price']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['price']
+    actions = [logical_delete]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add product'),
+                  {'fields': ['name', 'image', 'inventory', 'inavailable']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['name', 'image', 'inventory', 'inavailable']
+    list_filter = ['name', 'image', 'inventory', 'inavailable']
+    actions = [logical_delete]
 
 
 class DetailInline(admin.TabularInline):
-    model = Details
-    extra = 3
+    model = Detail
 
 
 class ProductModelAdmin(admin.ModelAdmin):
+    fieldsets = [(_('add product'),
+                  {'fields': ['name', 'image', 'inventory', 'inavailable','price','category','brand','discount']}),
+                 (_('delete_status'),
+                  {'fields': ['delete_time_stamp', 'is_deleted']})]
+
+    search_fields = ['name', 'image', 'inventory', 'inavailable']
+    list_filter = ['name', 'inventory', 'inavailable']
+    actions = [logical_delete]
     inlines = [DetailInline]
 
-admin.site.register(Details)
-admin.site.register(Product,ProductModelAdmin)
+
+
+
+admin.site.register(Discount, PriceAdmin)
+admin.site.register(Price, PriceAdmin)
+admin.site.register(DiscountCode, DiscountCodeAdmin)
+admin.site.register(Categori, CategoryAdmid)
+admin.site.register(Detail, DetailAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Product, ProductModelAdmin)
--- product/models.py
+++ product/models.py
@@ -1,44 +1,139 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-# Create your models here.
 from core.models import BaseModel
 
 
 class Discount(BaseModel):
-    pass
+    name = models.CharField(max_length=50,
+                            verbose_name=_('discount name'),
+                            help_text=_('enter the discount'))
+
+    currency = [('toman', 'toman'), ('percent', 'percent')]
+    discount = models.IntegerField(verbose_name=_('discount'),
+                                   help_text=_('enter the discount'),
+                                   validators=[],
+                                   choices=currency)
+
+    expiry_date =models.DateTimeField(verbose_name=_('expiry_date'),
+                                      help_text=_('enter the expiry date'))
+
+
+    def __str__(self):
+        return f'{self.id}#   {self.discount}'
 
 
 class Price(BaseModel):
-    pass
+    price = models.PositiveIntegerField(verbose_name=_('price'),
+                                        help_text=_('enter the price'),
+                                        blank=False,
+                                        null=False,
+                                        validators=[])
+
+    def __str__(self):
+        return f'{self.id}#   {self.price}'
 
 
 class DiscountCode(BaseModel):
-    pass
+    name = models.CharField(max_length=50,
+                            verbose_name=_('discount code'),
+                            help_text=_('enter name for discount'),
+                            )
+
+    code = models.IntegerField(unique=True,
+                               verbose_name=_('discount code'),
+                               help_text=' enter code for discount ')
+
+    expiry_date = models.DateTimeField(verbose_name=_('expiry_date'))
+
+    def __str__(self):
+        return f'{self.id}#  {self.name}'
 
 
-class Category(BaseModel):
-    pass
+class Categori(BaseModel):
+    parent = models.ForeignKey('self', on_delete=models.CASCADE,
+                               verbose_name=_('enter parent if exist'),
+                               default=None,
+                               null=True,
+                               blank=True)
+    name = models.CharField(max_length=100,
+                            verbose_name=_('category name'),
+                            unique=True,
+                            help_text=_('enter the category'))
+
+    def __str__(self):
+        return f'{self.id}# {self.name}'
 
 
-class Details(BaseModel):
-    product_detail = models.ForeignKey('Product', verbose_name=_('add details'),on_delete=models.CASCADE)
-    feature = models.CharField(max_length=50,verbose_name=_('feature'))
-    explain = models.CharField(max_length=50,verbose_name=_('explain'))
+class Detail(BaseModel):
+    product_detail = models.ForeignKey('Product',
+                                        verbose_name=_('product_detail'),
+                                        on_delete=models.CASCADE)
+
+    feature = models.CharField(max_length=50,
+                               verbose_name=_('feature'),
+                               blank=True,
+                               null=True)
+
+    explain = models.CharField(max_length=250,
+                               verbose_name=_('explain'),
+                               blank=True,
+                               null=True)
+
+    def __str__(self):
+        return f'{self.id}# {Product.name}'
+
+
+class Brand(BaseModel):
+    name = models.CharField(max_length=50,
+                            verbose_name=_('brand name'),
+                            help_text=_('enter the brand name'),
+                            unique=True)
+
+    def __str__(self):
+        return f'{self.id}# {self.name}'
 
 
 class Product(BaseModel):
+    brand = models.ForeignKey(Brand,
+                              verbose_name=_('barnd name'),
+                              on_delete=models.CASCADE)
+    category = models.ForeignKey(Categori,
+                                 verbose_name=_('category'),
+                                 on_delete=models.CASCADE,
+                                 help_text=_('enter the category'),
+                                 blank=False,
+                                 null=False)
+
+    price = models.ForeignKey(Price,on_delete=models.CASCADE,
+                              verbose_name=_('price'),
+                              help_text=_('enter the price'),
+                              blank=False,
+                              null=False)
+
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE,
+                              verbose_name=_('discount'),
+                              help_text=_('enter the discount'),
+                              blank=False,
+                              null=False)
+
+
+
     name = models.CharField(verbose_name=_('name'),
                             help_text=_('enter product name'),
                             max_length=200,
                             blank=False,
                             null=False)
 
-    Image = models.FileField(_('image'), upload_to='Product',
+    image = models.FileField(_('image'), upload_to=f'Product/',
                              help_text=_('put picture for product'),
                              blank=False,
-                             null=False)
+                             null=False, )
 
-    inventory = models.IntegerField(verbose_name=_('Inventory'),
-                                    blank=False,
-                                    null=False)
+    inventory = models.PositiveIntegerField(verbose_name=_('Inventory'),
+                                            blank=False,
+                                            null=False)
+
+    inavailable = models.BooleanField(verbose_name=_('inavailable'))
+
+    def __str__(self):
+        return f'{self.id}# {self.name}  cat :{self.category} ({self.inventory})'
