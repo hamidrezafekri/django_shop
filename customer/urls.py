8b515d637af5f2906cf1c4eb589bf18003435c99
hamidrezafekri <hamidrezafekri.dev@gmail.com>
2021-08-10 02:50:46 +0430

    delete

--- customer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from customer.views import *
-
-urlpatterns = [
-    path('userlist/', CustomerListApiView.as_view(), name='home'),
-    path('userdetail/<int:pk>', CustomerDetailApiView.as_view(), name='home'),
-    path('address/', AddressLoggedInUser.as_view(), name='home')
-]
